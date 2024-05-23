f41700716917d5bb557e0f6965a5625fd6278633
Ademir Júnior <adejr0220@gmail.com>
2024-05-23 15:07:04 -0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -6,7 +6,7 @@ def createCollaboratorsList(table_path):
     workbook = load_workbook(table_path)
     worksheet = workbook.active
     
-    colaboradores = []
+    collaborators = []
     
     for row in worksheet.iter_rows():
         for cell in row:
@@ -14,15 +14,18 @@ def createCollaboratorsList(table_path):
                 nomeColaborador = cell.offset(column=1).value
                 
                 colaborador = Collaborator(nomeColaborador,"","","","")
-                colaboradores.append(colaborador)
+                collaborators.append(colaborador)
     
-    return colaboradores
+    return collaborators
 
 def main():
     
-    createCollaboratorsList( # Caminho para a tabela excel
+    colaboradores = createCollaboratorsList( # Caminho para a tabela excel
         "./Pontomais_-_Jornada_(01.05.2024_-_14.05.2024)_-_44082604.xlsx"
     )
+    
+    for c in range(0, colaboradores.__len__()):
+        print(colaboradores[c].__str__())
 
 if __name__ == "__main__":
     main()
