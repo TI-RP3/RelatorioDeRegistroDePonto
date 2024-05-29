b517d0b2b906677fa949a828bd3395be8eefb91e
Ademir Júnior <adejr0220@gmail.com>
2024-05-29 19:04:44 -0300

    Delimitando a leitura ao período de dias

--- Main.py
+++ Main.py
@@ -17,25 +17,35 @@ def createCollaboratorsList(worksheet):
     return collaborators
 
 def createDailyLists(worksheet):
-    for row in worksheet.iter_rows(min_row=3):
+    workbook = load_workbook("./Pontomais_-_Jornada_(01.05.2024_-_14.05.2024)_-_44082604.xlsx")
+    worksheet = workbook.active
+    for i,row in enumerate(worksheet.iter_rows(min_row=3)):
+        if (row.__getitem__(0).value == "Colaborador"):
+            nomeColaborador = row.__getitem__(1).value
+            print(nomeColaborador)
+            skipRow = i + 2 + 3
+            
+        if (row.__getitem__(0).value == "Data" or 
+            row.__getitem__(0).value == "Resumo"):
+            skipRow = i = 1  + 3
+
+        if (row.__getitem__(0).value == "TOTAIS"):
+            skipRow = i + 2 + 3
+        
         for cell in row:            
-            if cell.value == "Colaborador":
+            """ if cell.value == "Colaborador":
                 nomeColaborador = cell.offset(column=1).value
-                headersCells = cell.row
-                print(nomeColaborador)
-            elif (
-                cell.value != "Data" and
-                cell.value != "TOTAIS" and
-                cell.value != "Resumo"
+                skipRow = cell.row + 1
+                print(nomeColaborador) """
+                
+            if (
+                (cell.column == 6 or 
+                cell.column == 7 or 
+                cell.column == 8 or 
+                cell.column == 9) and
+                cell.row > skipRow
             ):
-                if (
-                    (cell.column == 6 or 
-                    cell.column == 7 or 
-                    cell.column == 8 or 
-                    cell.column == 9) and
-                    cell.row > headersCells + 1
-                ):
-                    print(cell.value)
+                print(cell.value)
 
 def main():
     workbook = load_workbook("./Pontomais_-_Jornada_(01.05.2024_-_14.05.2024)_-_44082604.xlsx")
