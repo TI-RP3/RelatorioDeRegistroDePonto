a7e56de42a8c957f84377f4e45c51c3e53c9b988
Ademir Júnior <adejr0220@gmail.com>
2024-06-07 13:53:56 -0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -2,20 +2,6 @@ from openpyxl import load_workbook
 
 from Collaborator import Collaborator
 
-""" def createCollaboratorsList(worksheet):
-    
-    collaborators = []
-    
-    for row in worksheet.iter_rows():
-        for cell in row:
-            if cell.value == "Colaborador":
-                nomeColaborador = cell.offset(column=1).value # Pega o valor da célula na coluna seguinte (ao lado de "Colaborador")
-                
-                colaborador = Collaborator(nomeColaborador,firstEntry="",firstExit="",secondEntry="",secondExit="")
-                collaborators.append(colaborador) # Insere o novo colaborador ao final da lista de colaboradores
-    
-    return collaborators """
-
 def createDailyLists(worksheet):
     general_list = []
     collaborators_info = {}
@@ -47,8 +33,6 @@ def main():
     workbook = load_workbook("./Pontomais_-_Jornada_(01.05.2024_-_14.05.2024)_-_44082604.xlsx")
     worksheet = workbook.active
     
-    # colaboradores = createCollaboratorsList(worksheet)
-    
     general_list = createDailyLists(worksheet)
     
     print("[\n")
@@ -58,9 +42,6 @@ def main():
             print(collaborator.__str__() + "\n")
         print("\t],\n")
     print("]")
-    
-    """ for c in range(0, colaboradores.__len__()):
-        print(colaboradores[c].__str__()) """
 
 if __name__ == "__main__":
     main()
