80513c5e5c9e47b51de92ee19421ee5dda65ed65
Ademir Júnior <adejr0220@gmail.com>
2024-06-18 18:18:43 -0300

    :sparkles: feat: starting GUI

---
+++ application/app.py
@@ -0,0 +1,26 @@
+import eel
+import random
+from datetime import datetime
+
+import eel.edge
+
+eel.init('web')
+eel.start('index.html')
+
+@eel.expose
+def get_random_name():
+    eel.prompt_alerts('Random name')
+
+@eel.expose
+def get_random_number():
+    eel.prompt_alerts(random.randint(1, 100))
+
+@eel.expose
+def get_date():
+    eel.prompt_alerts(datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
+
+@eel.expose
+def get_ip():
+    eel.prompt_alerts('127.0.0.1')
+
+eel.start('index.html')
